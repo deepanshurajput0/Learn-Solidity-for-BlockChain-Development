f9931c79d50f60ee3ac0a08dd214abb4add46d13
deepanshurajput0 <deepanshuverma186@gmail.com>
2024-08-24 20:28:22 -0700

    use case of view, pure, public

--- functions.sol
+++ functions.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.15;
 
 
 contract functions{
-    uint public  val = 4;
-    function add() public  returns (uint){
-         
+    uint public  val = 5;
+    function add() public   returns (uint){
+         return val+= 1 ;
     }
 }
+
+// pure - state varibales, state, edit
+
+// view - state variables view 
