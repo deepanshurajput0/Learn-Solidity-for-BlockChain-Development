b5b155ea257e50f6b9bf05f9293e0088c8e3ccce
deepanshurajput0 <deepanshuverma186@gmail.com>
2024-08-24 19:00:18 -0700

    functions

---
+++ basic.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract variables {
+    // State variables 
+    string public name = "hello world";
+    bool public  easy = true;
+    uint videos = 30;
+    bytes4 symbol = "web3";
+    int8 playlist = 127;
+    address add = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
+
