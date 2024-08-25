71651e0efc1b4509791481a84b05a508393416cc
deepanshurajput0 <deepanshuverma186@gmail.com>
2024-08-25 14:29:07 -0700

    events

---
+++ events.sol
@@ -0,0 +1,11 @@
+pragma solidity  ^0.8.20;
+
+contract Loop {  
+  event transfer( address indexed  _from, address _to, uint _amount );
+  function trans(address to, uint amount) public  {
+     emit transfer(msg.sender,to,amount); 
+  }
+}
+
+
+
